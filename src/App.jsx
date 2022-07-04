025f083ea90c2226f4018aa4211592504ec3f657
Harsh Shaktawat <shaktawatharsh19@gmail.com>
2022-07-05 02:35:01 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import CreateArea from "./components/CreateArea";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/Note";
+//import notes from "./notes";
+function App(){
+    const [notes,setNotes]=React.useState([]); 
+    function addNote(newNote) {
+        setNotes(prevNotes=>{
+            return [...prevNotes,newNote];
+        });
+        //console.log(newNote);
+    }
+    function deleteNote(id) {
+        setNotes(prevNotes=>{
+            return prevNotes.filter((noteItem,index)=>{
+                return index!==id;
+            });
+        }); 
+    }
+    return (
+        <div>
+            <Header/>
+            <CreateArea onAdd={addNote}/>
+            {notes.map((noteItem,index)=>(
+                <Note
+                    key={index}
+                    id={index}
+                    title ={noteItem.title}  
+                    content={noteItem.content}
+                    onDelete={deleteNote}
+                />
+            ))}
+            <Footer/>
+        </div>
+    )
+}
+export default App;
